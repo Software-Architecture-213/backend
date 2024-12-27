362bf006114c80ed7f5cffa90d475e42fc66c84b
DucMinh <flashback054@gmail.com>
2024-12-27 19:10:56 +0700

    [Minh]feat: mongoose models to match schemas; SQL scripts

---
+++ game-service/models/ItemTransaction.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+
+const ItemTransactionSchema = new mongoose.Schema(
+	{
+		_id: { type: String, default: uuidv4 }, // UUID
+		senderId: { type: String }, // Người gửi
+		receiverId: { type: String }, // Người nhận
+		itemId: { type: String, ref: "Item" }, // Vật phẩm được gửi
+		expiredAt: { type: Date }, // Ngày hết hạn
+		status: {
+			type: String,
+			enum: ["pending", "accepted", "rejected"],
+			default: "pending",
+		}, // Trạng thái giao dịch
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("ItemTransaction", ItemTransactionSchema);
